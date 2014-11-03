1bd0a7a86127fd911db17aaf5b9512e58e97b29f
Vanessa <vsochat@stanford.edu>
2014-11-03 10:53:19 -0800

    modified:   examples/single_gene_analysis.sh

--- examples/single_gene_analysis.sh
+++ examples/single_gene_analysis.sh
@@ -2,7 +2,7 @@
 
 # Compare map dso_0060131 with gene AKR7A3, output data table as well as results file
 GENE=AKR7A3
-/home/vanessa/Documents/Work/GENE_EXPRESSION/alleninf
+OUTDIR=/home/vanessa/Documents/Work/GENE_EXPRESSION/alleninf
 alleninf --gene $GENE --radius 3 --data True /home/vanessa/Documents/Work/BRAINBEHAVIOR/DisorderMaps/dso_0060046_pAgF_z_FDR_0.05.nii.gz $OUTDIR/$GENE"_alleninf.tsv"
 
 GENE=AKAP4
